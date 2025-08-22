85e7a67fc211ed90747864a5cba9301c873c9878
Shashank <shashankr1999@gmail.com>
2025-08-22 21:51:15 +0530

    added remove-button and checkbox toggle functionality on top of the base react jsx code

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -14,27 +14,53 @@ function App() {
       isCompleted: false,
     };
 
-    console.log("old", tasks);
-
     tasks.unshift(newTask);
+    
     const newTasks = [...tasks];
-
-    console.log("new", tasks);
     setTasks(newTasks);
+    setTaskTitle(''); // Clear the input field 
   }
 
   function handleTaskInput(event) {
-    console.log("input fired");
     setTaskTitle(event.target.value);
+  }
+
+  // function to remove a task
+  function handleRemoveTask(index) {
+    const newTasks = [];
+    for (let i = 0; i < tasks.length; i++) {
+      if (i !== index) {
+        newTasks.push(tasks[i]);
+      }
+    }
+    setTasks(newTasks); // Update tasks state
+  }
+
+  // function to handle checkbox
+  function handleToggleTask(index, event) {
+    const newTasks = [];
+    for (let i = 0; i < tasks.length; i++) {
+      if (i === index) {
+        const newIsCompleted = event.target.checked;
+        newTasks.push({
+          title: tasks[i].title,
+          isCompleted: newIsCompleted
+        });
+      } else {
+        // Copy task unchanged
+        newTasks.push(tasks[i]);
+      }
+    }
+    setTasks(newTasks); // Update tasks state
   }
 
   return (
     <>
       <header>
-        <h1>Best TODO App</h1>
+        <h1 className="header">React TODO App</h1>
       </header>
 
-      <form>
+      <form className="todo-form">
         <input
           type="text"
           className="input"
@@ -44,16 +70,29 @@ function App() {
         <button onClick={handleAddTask}>Add task</button>
       </form>
 
-      <ol id="js-task-list">
-        {tasks.map(function (task) {
+      <ol className="task-list" id="js-task-list">
+        {tasks.map(function (task, index) {
           return (
-            <li>
-              <label>
-                <input type="checkbox" />
-                {task.title}
+            <li className="js-task-list-item" key={index}>
+              <label className="js-task-label">
+                <input 
+                type="checkbox" 
+                className="js-task-checkbox"
+                checked={task.isCompleted}
+                  onChange={function(event) {
+                    handleToggleTask(index, event);
+                  }}                
+                />
+                <span className="js-task-title">{task.title}</span>
               </label>
 
-              <button type="button">Remove</button>
+              <button 
+              type="button" 
+              className="js-remove-btn"
+              onClick={function() {
+                  handleRemoveTask(index);
+                }}              
+              >Remove</button>
             </li>
           );
         })}
